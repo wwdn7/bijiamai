5b3526b34937dec0a4fdde7e35e80a217429db0d
wwdn7 <wwdn7@vip.qq.com>
2018-02-14 17:16:50 +0800

    2018-02-14 17:16

--- mmb/js/index.js
+++ mmb/js/index.js
@@ -24,9 +24,14 @@ Mmb.prototype = {
     $.ajax({
       url: 'http://127.0.0.1:9090/api/getmoneyctrl',
       success: function (data) {
-        // console.log(data);
+        // 将评论数中的数字提取出来
+        for(var i = 0; i < data.result.length; i++){
+          data.result[i].productComCount = +(data.result[i].productComCount.match(/\d+/)[0]);
+        }
+        console.log(data);
+        
         var html = template('productlistTmp', data);
-        console.log(html);
+        // console.log(html);
         $('#main .discount-list ul').html(html);
       }
     })
